d0a427e5640c97c5f980206e9edca7e9cf22637d
dark2git <fatalerror14.88@icloud.com>
2025-03-26 18:50:09 +0100

    remote commit

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,11 @@
-const quantity = 10;
-const pricePerDroid = 10;
-
-function makeTransaction() {
-  console.log('Log inside multiply function');
+"use strict";
+function makeTransaction(quantity, pricePerDroid) {
+    const totalPrice = quantity * pricePerDroid;
+    return `You ordered ${quantity}, droids worth  ${totalPrice} credits!`;
 }
+
+
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+
